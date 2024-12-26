bcbe68057e0dfa70911aa5acd37c720d48fd0c66
Richard Saney <rnsaney@gmail.com>
2024-12-25 17:14:13 -0700

    Use Google style C++ formatting.
    In VSCode: Configure the Formatting Style
    The C++ formatting options is "C_Cpp.clang_format_fallbackStyle".
    Set this to: "Google".

--- src/textadventure-main.cc
+++ src/textadventure-main.cc
@@ -1,17 +1,26 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
-using namespace std;
+int main() {
+  std::vector<std::string> msg{"Hello", "C++",     "World",
+                               "from",  "VS Code", "and the C++ extension!"};
 
-int main()
-{
-    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+  for (const std::string &word : msg) {
+    std::cout << word << " ";
+    std::cout.flush();
+  }
+  std::cout << std::endl;
 
-    for (const string &word : msg)
-    {
-        cout << word << " ";
-        cout.flush();
+  while (true) {
+    std::string input;
+    std::cout << "Enter: ";
+    std::cout.flush();
+    std::getline(std::cin, input);
+    if (!input.empty()) {
+      std::cout << "You wrote: " << input << std::endl;
+      break;
     }
-    cout << endl;
+  }
+  return 0;
 }
