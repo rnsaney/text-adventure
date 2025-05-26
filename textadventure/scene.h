57cb8dded3654ae7f6e1e0141b907b9e79227031
Richard Saney <rnsaney@gmail.com>
2025-05-25 17:20:22 -0700

    Update scene description.

--- textadventure/scene.h
+++ textadventure/scene.h
@@ -12,13 +12,16 @@ namespace game {
 class State;
 struct ActionStruct;
 
+// A room or area in the adventure.
+//
+// The scene will have actions that may progress to another scene.
 class Scene {
  public:
   virtual ~Scene() = default;
 
   virtual std::string Name() const = 0;
 
-  // Return the description of the scene.
+  // Return the description to print at scene entry.
   virtual std::string Description() const = 0;
 
   void PrintDescription() const;
